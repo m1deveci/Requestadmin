dd4feafce47fe9f70b26a9626ccde17bc01c1db7
Mustafa Deveci <m1deveci91@gmail.com>
2025-09-06 17:42:06 +0300

    Convert PHP Request Admin to React + Node.js

--- employee/edit_request.php
+++
@@ -1,9 +0,0 @@
-<?php
-$requestId = $_GET['id'] ?? '';
-if (!empty($requestId)) {
-    header('Location: request_detail.php?id=' . $requestId . '&edit=1');
-} else {
-    header('Location: requests.php');
-}
-exit;
-?>
